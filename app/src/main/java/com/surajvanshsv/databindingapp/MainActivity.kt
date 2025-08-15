4c175432a7f97403503ed5959587d2df1e480d92
Suraj Kumar <sk658139@gmail.com>
2025-08-15 14:08:24 +0530

    initial commit

---
+++ app/src/main/java/com/surajvanshsv/databindingapp/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.surajvanshsv.databindingapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.databinding.DataBindingUtil
+import com.surajvanshsv.databindingapp.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+//        setContentView(R.layout.activity_main)
+
+        //use data binding util to set the content view with data binding
+        binding = DataBindingUtil.setContentView(
+            this,R.layout.activity_main
+        )
+
+        binding.textview1.text = "hello this is test"
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
